1a8a686c984cc8064f3f6094961c85de5e49fea1
Håvard Karlsen Solheim <haavardkso@uia.no>
2025-10-01 18:00:18 +0200

    Done with explosion, ish. Has a weird visual bug at the end of the animation with the flare and streaks

---
+++ World/explosion.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("start_animation"):
+		anim_player.play("Init")
