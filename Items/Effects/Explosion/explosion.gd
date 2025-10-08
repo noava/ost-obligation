d784b9ee285111da5345fda9e6792c98af5122fa
Håvard Karlsen Solheim <haavardkso@uia.no>
2025-10-08 16:38:41 +0200

    Moved explosion to /Items/Effects/Explosion from /World, also made it to a scene.

--- Items/Effects/Explosion/explosion.gd
+++ Items/Effects/Explosion/explosion.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var anim_player: AnimationPlayer = $Explosion_Animation
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("start_animation"):
