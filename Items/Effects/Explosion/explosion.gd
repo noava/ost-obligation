8bf4b67db2308dc6fbef9b281e5bf6446acddbb2
Håvard Karlsen Solheim <haavardkso@uia.no>
2025-10-08 16:58:02 +0200

    Merged with main, and added explosion as a scene to testing world

--- Items/Effects/Explosion/explosion.gd
+++ Items/Effects/Explosion/explosion.gd
@@ -3,5 +3,5 @@ extends Node3D
 @onready var anim_player: AnimationPlayer = $Explosion_Animation
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("start_animation"):
+	if Input.is_action_just_pressed("explosion"):
 		anim_player.play("Init")
